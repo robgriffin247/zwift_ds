6c37e319e076bff6f81d6598a2a9c1d5564e8906
Rob Griffin <robgriffin247@hotmail.com>
2026-09-11 08:53:42 +0200

    added dlt loading to a duckdb database and updated readme with stack diagram

--- ingestion/zwift_racing.py
+++ ingestion/zwift_racing.py
@@ -1,11 +1,24 @@
 import httpx
 import os
 import json
+import dlt
+from dlt.extract import DltResource
 
 BASE_URL = "https://api.zwiftracing.app/api/public/"
 HEADER = {"Authorization": os.getenv("ZWIFT_RACING_API_KEY")}
 
+PIPELINE = dlt.pipeline(
+    pipeline_name=f"zwift_ds_pipeline",
+    destination=dlt.destinations.duckdb(f"data/zwift_ds_{os.getenv('TARGET')}.duckdb"),
+    dataset_name="raw_zwift_racing",
+)
 
+
+@dlt.resource(
+    name="riders",
+    write_disposition="merge",
+    primary_key="rider_id",
+)
 def get_rider(rider_id: int, use_json=False):
     if use_json:
         with open("data/raw/rider.json", "r") as f:
@@ -19,6 +32,11 @@ def get_rider(rider_id: int, use_json=False):
     yield rider
 
 
+@dlt.resource(
+    name="riders",
+    write_disposition="merge",
+    primary_key="rider_id",
+)
 def get_riders(rider_ids: list[int], use_json=False):
     if use_json:
         with open("data/raw/riders.json", "r") as f:
@@ -31,6 +49,11 @@ def get_riders(rider_ids: list[int], use_json=False):
     yield from riders
 
 
+@dlt.resource(
+    name="riders",
+    write_disposition="merge",
+    primary_key="rider_id",
+)
 def get_club_riders(club_id: int, use_json=False):
     if use_json:
         with open("data/raw/club_riders.json", "r") as f:
@@ -43,19 +66,28 @@ def get_club_riders(club_id: int, use_json=False):
     yield from riders
 
 
+def run_pipeline(resource: DltResource) -> None:
+    load_info = PIPELINE.run(resource)
+    return load_info
+
+
 if __name__ == "__main__":
-    OFFLINE = True
+    OFFLINE = False
 
-    os.makedirs("data/raw", exist_ok=True)
+    # os.makedirs("data/raw", exist_ok=True)
 
-    rider = [r for r in get_rider(4598636, use_json=OFFLINE)]
-    with open("data/raw/rider.json", "w") as f:
-        json.dump(rider, f)
+    # rider = [r for r in get_rider(4598636, use_json=OFFLINE)]
+    # with open("data/raw/rider.json", "w") as f:
+    #     json.dump(rider, f)
 
-    riders = [r for r in get_riders([4598636, 5574], use_json=OFFLINE)]
-    with open("data/raw/riders.json", "w") as f:
-        json.dump(riders, f)
+    # riders = [r for r in get_riders([4598636, 5574], use_json=OFFLINE)]
+    # with open("data/raw/riders.json", "w") as f:
+    #     json.dump(riders, f)
 
-    club_riders = [r for r in get_club_riders(20650, use_json=OFFLINE)]
-    with open("data/raw/club_riders.json", "w") as f:
-        json.dump(club_riders, f)
+    # club_riders = [r for r in get_club_riders(20650, use_json=OFFLINE)]
+    # with open("data/raw/club_riders.json", "w") as f:
+    #     json.dump(club_riders, f)
+
+    print(run_pipeline(get_rider(4598636, use_json=OFFLINE)))
+    print(run_pipeline(get_riders([4598636], use_json=OFFLINE)))
+    print(run_pipeline(get_club_riders(20650, use_json=OFFLINE)))
