c37d950a2a3e7a04c6b0cbd090dfb346e7e3e944
Rob Griffin <robgriffin247@hotmail.com>
2026-09-18 10:54:11 +0200

    added auth

--- data_api/README.md
+++ data_api/README.md
@@ -9,3 +9,9 @@ uv run uvicorn data_api.app:app --reload --port 8000
 ```
 curl 127.0.0.1:8000
 ```
+
+- Generate an API token and then add it to the .env/modal secrets under ``ZWIFT_DS_API_TOKEN``
+
+```
+openssl rand -hex 32
+```
--- data_api/app.py
+++ data_api/app.py
@@ -1,15 +1,23 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends, status
+from fastapi.security import APIKeyHeader
 import duckdb
 from enum import Enum
 import os
+import secrets
 
 app = FastAPI()
 
+API_TOKEN = os.getenv("ZWIFT_DS_API_TOKEN")
 DATABASE = f"md:zwift_ds_prod?motherduck_token={os.getenv('MOTHERDUCK_TOKEN')}" if os.getenv("TARGET")=="prod" else "data/zwift_ds_dev.duckdb" 
+
+api_key_header = APIKeyHeader(name="X-API-Key")
 
 class CoreTables(str, Enum):
     riders = "riders"
 
+async def verify_token(api_key: str = Depends(api_key_header)) -> None:
+    if not API_TOKEN or not secrets.compare_digest(api_key, API_TOKEN):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid API key")
 
 @app.get("/")
 async def root():
@@ -17,8 +25,8 @@ async def root():
 
 
 @app.get("/{table}")
-async def get_table(table: CoreTables):
+async def get_table(table: CoreTables, _: None = Depends(verify_token)):
     with duckdb.connect(DATABASE) as con:
-        data = con.sql(f"select * from core.{table.value}").pl()
+        data = con.sql(f"select * from core.{table.value} where rider_id=4598636").pl()
 
     return {"content": data.to_dicts()}
